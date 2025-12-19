3f17e61763d2a1dd375fcdbadf200cd0ce8f8515
electr1fy0 <electr1fy0@icloud.com>
2025-12-19 13:32:52 +0530

    move to low level functions

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #define KEY_SIZE 32
 #define VALUE_SIZE 256
@@ -10,43 +13,52 @@ typedef struct {
 } Record;
 
 int db_insert(const char *filename, const char *key, const char *value) {
-  FILE *fp = fopen(filename, "abc");
-  if (!fp) return -1;
+  int fd = open("db.bin", O_WRONLY | O_CREAT | O_APPEND, 0644);
 
   Record r = {0};
   strncpy(r.key, key, KEY_SIZE - 1);
   strncpy(r.value, value, VALUE_SIZE - 1);
 
-  fwrite(&r, sizeof(Record), 1, fp);
-  fclose(fp);
-
+  ssize_t total = 0;
+  while (total < sizeof(Record)) {
+    ssize_t n = write(fd, ((char *)&r) + total, sizeof(Record) - total);
+    if (n <= 0) return -1;
+    total += n;
+  }
+  close(fd);
   return 0;
 }
 
 int db_get(const char *filename, const char *key, char *out_value) {
-  FILE *fp = fopen(filename, "rb");
-  if (!fp) return -1;
+
+  int fd = open("db.bin", O_RDONLY);
 
   Record r;
 
-  while (fread(&r, sizeof(Record), 1, fp)) {
+  while (1) {
+    ssize_t n = read(fd, &r, sizeof(Record));
+    if (n == 0) break;
+    if (n < 0) return -1;
+
+    if (n != sizeof(Record) || n < 0) {
+      close(fd);
+      return -1;
+    }
+
     if (strncmp(r.key, key, KEY_SIZE) == 0) {
       strncpy(out_value, r.value, VALUE_SIZE);
-      fclose(fp);
+      close(fd);
       return 0;
     }
   }
-  fclose(fp);
+  close(fd);
   return -1;
 }
-#include <stdio.h>
 
 int main() {
-    db_insert("db.bin", "name", "ayush");
-    db_insert("db.bin", "lang", "c");
+  db_insert("db.bin", "name", "ayush");
+  db_insert("db.bin", "lang", "c");
 
-    char value[VALUE_SIZE];
-    if (db_get("db.bin", "lang", value) == 0) {
-        printf("lang = %s\n", value);
-    }
+  char value[VALUE_SIZE];
+  if (db_get("db.bin", "lang", value) == 0) { printf("lang = %s\n", value); }
 }
